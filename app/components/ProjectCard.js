c4c339d94a4a57b38021a428d6cbbe6449ed4408
DwipshikhaLodh <lodhdwipshikha@gmail.com>
2023-10-12 19:02:06 +0530

    changes made

--- app/components/ProjectCard.js
+++ app/components/ProjectCard.js
@@ -6,14 +6,14 @@ import { BiLinkAlt } from 'react-icons/bi'
 function ProjectCard({ image, title, desc, tech, link}) {
     return (
         <div className={styles.projectCard }>
-            <Image className={styles.image} src={image} alt="project image" height="100%" width="100%"></Image>
+            <Image className={styles.image} src={image} alt="project image" height={200} width={200}></Image>
             <div className={styles.linkContainer}>
             <Link className={styles.link} href={link}><BiLinkAlt/></Link> 
             </div>
             <p>{title}</p>
             <div className={styles.desc}>
                     {desc}
-                </div>
+            </div>
         </div>
     )
 }
